b78b0d6ccbec957298323c38ef474176af49f860
Marco Täschner <marco.taeschner@me.com>
2017-11-07 11:20:23 +0100

    Bugfix

--- src/main/java/com/exxeta/clouddemo/CloudDemoApplication.java
+++ src/main/java/com/exxeta/clouddemo/CloudDemoApplication.java
@@ -70,6 +70,31 @@ class Customer {
         this.lastName = lastName;
     }
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+    
+
     @Override
     public String toString() {
         return String.format(
