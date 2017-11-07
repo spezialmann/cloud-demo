6200a507f70d865f2b3455fed1031af5c2bf0e06
Marco Täschner <marco.taeschner@me.com>
2017-11-07 09:26:19 +0100

    Add DB features

--- src/main/java/com/exxeta/clouddemo/CloudDemoApplication.java
+++ src/main/java/com/exxeta/clouddemo/CloudDemoApplication.java
@@ -1,7 +1,16 @@
 package com.exxeta.clouddemo;
 
+import java.util.List;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,16 +19,66 @@ import org.springframework.web.bind.annotation.RequestParam;
 @SpringBootApplication
 public class CloudDemoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CloudDemoApplication.class, args);
-	}
+    private static final Logger log = LoggerFactory.getLogger(CloudDemoApplication.class);
+    
+    public static void main(String[] args) {
+        SpringApplication.run(CloudDemoApplication.class, args);
+    }
 }
 
 @Controller
 class HelloController {
+
     @RequestMapping("/hello")
-    public String hello(Model model, @RequestParam(value="name", required=false, defaultValue="World") String name) {
+    public String hello(Model model, @RequestParam(value = "name", required = false, defaultValue = "World") String name) {
         model.addAttribute("name", name);
         return "hello";
     }
+}
+
+@Controller
+class DbController {
+    
+    @Autowired
+    CustomerRepository customerRepository;
+
+    @RequestMapping("/db")
+    public String hello(Model model) {
+        
+        Customer customer = new Customer("Max", "Mustermann");
+        customerRepository.save(customer);
+        
+        model.addAttribute("customer", customerRepository.findAll());
+        return "hello";
+    }
+}
+
+@Entity
+class Customer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String firstName;
+    private String lastName;
+
+    protected Customer() {
+    }
+
+    public Customer(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    @Override
+    public String toString() {
+        return String.format(
+                "Customer[id=%d, firstName='%s', lastName='%s']",
+                id, firstName, lastName);
+    }
+
+}
+
+interface CustomerRepository extends CrudRepository<Customer, Long> {
+    List<Customer> findByLastName(String lastName);
 }
